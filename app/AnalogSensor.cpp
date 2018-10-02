16307b2008fe84468fd5f6620e85a6be74a1ace9
Learner1729 <ashishpatel0894@gmail.com>
2018-10-02 16:59:46 -0400

    Performed Static code analysis using cppcheck & cpplint and modifed readme.file

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -4,21 +4,22 @@
 #include <vector>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+  : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
-    // Using smart pointers intead of traditional pointers, so that we don't need to use delete keyword.
-    // Secondly, using make_shared to avoid explicit use of new and to have only one dynamic memory allocation.
-    std::shared_ptr<std::vector<int>> readings(std::make_shared<std::vector<int>>(mSamples, 10));
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
-    return result;
+int AnalogSensor::Read() {
+  // Using smart pointers intead of traditional pointers,
+  // so that we don't need to use delete keyword.
+  // Secondly, using make_shared to avoid explicit use of new and
+  // to have only one dynamic memory allocation.
+  std::shared_ptr<std::vector<int>>
+    readings(std::make_shared<std::vector<int>>(mSamples, 10));
+  double result =
+    std::accumulate(readings->begin(), readings->end(), 0.0) / readings->size();
+  return result;
 }
 
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <AnalogSensor.hpp>
 
-int main()
-{
-    AnalogSensor lightSensor(5);
-    std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    // Initialized the variable to resolve the error
-    auto terminator{true};
-    if( terminator )
-    {
-        std::cout << "DONE" << std::endl;
-    }
-    return 0;
+int main() {
+  AnalogSensor lightSensor(5);
+  std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
+  // Initialized the variable to resolve the error
+  auto terminator{true};
+  if (terminator) {
+    std::cout << "DONE" << std::endl;
+  }
+  return 0;
 }
